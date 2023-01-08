f26e944307f80fb7b9ed40acfa79a593ed3bc391
raresdumitrescu30 <116021547+raresdumitrescu30@users.noreply.github.com>
2023-01-08 18:27:55 +0200

    Add files via upload

---
+++ src/components/RobotList.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import RobotStore from '../stores/RobotStore'
+import Robot from './Robot'
+import RobotForm from './RobotForm'
+
+
+
+class RobotList extends Component {
+	constructor(){
+		super()
+		this.state = {
+			robots : []
+		}
+		this.addRobot = robot => {
+			this.store.addRobot(robot);
+		};
+	}
+	componentDidMount(){
+		this.store = new RobotStore()
+		this.setState({
+			robots : this.store.getRobots()
+		})
+		this.store.emitter.addListener('UPDATE', () => {
+			this.setState({
+				robots : this.store.getRobots()
+			})			
+		})
+	}
+	render() {
+		return (
+			<div>
+				 
+				{
+					this.state.robots.map((e, i) => 
+						<Robot item={e} key={i} />
+					)
+				}
+				<RobotForm onAdd={this.addRobot}/>
+			</div>
+		)
+	}
+}
+
+export default RobotList
